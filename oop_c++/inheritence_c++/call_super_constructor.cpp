2ecbf19a9bcf220216fff8ba8753dca3b58c55de
Blake Conrad <conradbm@uindy.edu>
2017-04-24 23:36:37 -0400

    minor changes

---
+++ oop_c++/inheritence_c++/call_super_constructor.cpp
@@ -0,0 +1,22 @@
+/* Another great example */
+
+class SuperClass
+{
+    public:
+
+        SuperClass(int foo)
+        {
+            // do something with foo
+        }
+
+class SubClass : public SuperClass
+{
+    public:
+
+        SubClass(int foo, int bar)
+        : SuperClass(foo)    // Call the superclass constructor in the 
+subclass' initialization list.
+        {
+            // do something with bar
+        }
+};
--- oop_c++/inheritence_c++/polymorphism.cpp
+++ oop_c++/inheritence_c++/polymorphism.cpp
@@ -67,26 +67,3 @@ int main( ) {
    return 0;
 }
 
-/* Another great example */
-
-class SuperClass
-{
-    public:
-
-        SuperClass(int foo)
-        {
-            // do something with foo
-        }
-};
-
-class SubClass : public SuperClass
-{
-    public:
-
-        SubClass(int foo, int bar)
-        : SuperClass(foo)    // Call the superclass constructor in the 
-subclass' initialization list.
-        {
-            // do something with bar
-        }
-};
