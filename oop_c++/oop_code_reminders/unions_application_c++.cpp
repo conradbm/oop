33f210cd4715b70012389a6824555cc764d7c53c
Blake Conrad <bmconrad@iupui.edu>
2018-01-31 12:25:36 -0600

    Add files via upload

---
+++ oop_c++/oop_code_reminders/unions_application_c++.cpp
@@ -0,0 +1,82 @@
+# Using unions 
+# Syntax: union [name]  { member-list };  
+
+#include "stdafx.h"  
+#include <queue>  
+  
+using namespace std;  
+  
+enum class WeatherDataType  
+{  
+    Temperature, Wind  
+};  
+  
+struct TempData  
+{  
+    int StationId;  
+    time_t time;  
+    double current;  
+    double max;  
+    double min;  
+};  
+  
+struct WindData  
+{  
+    int StationId;  
+    time_t time;  
+    int speed;  
+    short direction;  
+};  
+  
+struct Input  
+{  
+    WeatherDataType type;  
+    union  
+    {  
+        TempData temp;  
+        WindData wind;  
+    };  
+};  
+  
+// Functions that are specific to data types  
+void Process_Temp(TempData t) {}  
+void Process_Wind(WindData w) {}  
+  
+// Container for all the data records  
+queue<Input> inputs;  
+void Initialize();  
+  
+int main(int argc, char* argv[])  
+{  
+    Initialize();  
+    while (!inputs.empty())  
+    {  
+        Input i = inputs.front();  
+        switch (i.type)  
+        {  
+        case WeatherDataType::Temperature:  
+            Process_Temp(i.temp);  
+            break;  
+        case WeatherDataType::Wind:  
+            Process_Wind(i.wind);  
+            break;  
+        default:  
+            break;  
+        }  
+        inputs.pop();  
+  
+    }  
+    return 0;  
+}  
+  
+void Initialize()  
+{  
+    Input first, second;  
+    first.type = WeatherDataType::Temperature;  
+    first.temp = { 101, 1418855664, 91.8, 108.5, 67.2 };  
+    inputs.push(first);  
+  
+    second.type = WeatherDataType::Wind;  
+    second.wind = { 204,1418859354, 14, 27 };  
+    inputs.push(second);  
+}  
